8bcc41bbdb28a63264dc798cf073569dbc5a9f5d
Yogesh Sonawane <yogesh-sonawane@users.noreply.github.com>
2019-05-29 17:28:50 +0530

    server file changes

---
+++ flo-kapture-nodejs-server-app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+var app = express();
+app.listen(3000, "127.0.0.1", function () {
+    console.log("floKapture Server Application is up and running now...");
+});
